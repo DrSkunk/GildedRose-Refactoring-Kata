a53948bdba859a78c72af3a8dd01e568bf05c0b4
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2021-10-08 09:31:40 +0200

    Move item handling to individual functions

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -29,60 +29,72 @@ export class GildedRose {
     );
   }
 
+  static handleSulfuras(item: Item): Item {
+    // Sulfuras quality is always 80
+    item.quality = 80;
+    return item;
+  }
+
+  static handleAgedBrie(item: Item): Item {
+    item.quality = Math.max(
+      GildedRose.minQuality,
+      Math.min(item.quality + 1, GildedRose.maxQuality)
+    );
+    return item;
+  }
+
+  // Backstage passes increase in quality over time
+  // Unless the concert is over
+  static handleBackstagePasses(item: Item): Item {
+    let increase;
+    if (item.sellIn > 10) {
+      increase = 1;
+    } else if (item.sellIn > 5) {
+      increase = 2;
+    } else if (item.sellIn >= 0) {
+      increase = 3;
+    } else {
+      item.quality = 0;
+      return item;
+    }
+    item.quality = GildedRose.limitQuality(item.quality + increase);
+    return item;
+  }
+
+  static handleNormalItem(item: Item): Item {
+    // Once the sell by date has passed, Quality degrades twice as fast
+    const degradeFactor = item.sellIn < 0 ? 2 : 1;
+
+    // "Conjured" items degrade in Quality twice as fast as normal items
+    if (item.name.toLowerCase().indexOf("conjured") !== -1) {
+      item.quality = GildedRose.limitQuality(item.quality - degradeFactor * 2);
+      return item;
+    }
+    // Normal item
+    item.quality = GildedRose.limitQuality(item.quality - degradeFactor * 1);
+    return item;
+  }
+
   updateQuality(): Item[] {
     this.items = this.items.map((item) => {
-      // Lower sell by date, with Sulfuras being the only exception as it never alters
+      // Lower sellIn date, with Sulfuras being the only exception as it never alters
       if (item.name === "Sulfuras, Hand of Ragnaros") {
-        // Sulfuras quality is always 80
-        item.quality = 80;
-        return item;
+        return GildedRose.handleSulfuras(item);
       }
+      // First the sellIn date is reduced before calculating the new quality
       item.sellIn--;
 
-      // Items that have better quality over time
+      // Two items improve in quality over time
       if (item.name === "Aged Brie") {
-        item.quality = Math.max(
-          GildedRose.minQuality,
-          Math.min(item.quality + 1, GildedRose.maxQuality)
-        );
-        return item;
+        return GildedRose.handleAgedBrie(item);
       }
-      // Backstage passes increase in quality over time
-      // Unless the concert is over
-      // if "lib": ["es2017"] is added to tsconfig.json includes() can be used instead
+      // if "lib": ["es2017"] is added to tsconfig.json, includes() can be used instead
       if (item.name.toLowerCase().indexOf("backstage passes") !== -1) {
-        let increase;
-        if (item.sellIn > 10) {
-          increase = 1;
-        } else if (item.sellIn > 5) {
-          increase = 2;
-        } else if (item.sellIn >= 0) {
-          increase = 3;
-        } else {
-          item.quality = 0;
-          return item;
-        }
-        item.quality = GildedRose.limitQuality(item.quality + increase);
-        return item;
+        return GildedRose.handleBackstagePasses(item);
       }
-      // Once the sell by date has passed, Quality degrades twice as fast
-      let degradeFactor = 1;
-      if (item.sellIn < 0) {
-        degradeFactor = 2;
-      }
-
-      // "Conjured" items degrade in Quality twice as fast as normal items
-      if (item.name.toLowerCase().indexOf("conjured") !== -1) {
-        item.quality = GildedRose.limitQuality(
-          item.quality - degradeFactor * 2
-        );
-        return item;
-      }
-      // Normal item
-      item.quality = GildedRose.limitQuality(item.quality - degradeFactor * 1);
-      return item;
+      // All special items are handled
+      return GildedRose.handleNormalItem(item);
     });
-
     return this.items;
   }
 }
